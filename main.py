73ffa81387895122928e1ca682b2dabc3805a0ba
ohyeah <davletiyarov@gmail.com>
2021-06-16 17:28:01 +0600

    changing order, overall works

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ from pytz import timezone
 import telebot
 from telebot.types import ChatPermissions
 
-TOKEN = 'xxx'
+TOKEN = '1647052009:AAH4WbN11l_ty7178cA47wnQsIKWfN91wgY'
 
-# bot = telebot.TeleBot(TOKEN)
 u = Updater(TOKEN)
 
 j = u.job_queue
 # Ask your Peer advisor chat_id: -1001258717612
+
+# @bot.message_handler(commands=['open'])
+def openchat(context: telegram.ext.CallbackContext):
+    context.bot.set_chat_permissions("-1001258717612", ChatPermissions(can_send_messages=True, can_send_media_messages=True,
+                                                               can_send_polls=True, can_send_other_messages=True,
+                                                               can_add_web_page_previews=True,
+                                                               can_change_info=None, can_invite_users=True,
+                                                               can_pin_messages=None))
+    context.bot.send_message("-1001258717612", "Monday morning. Chat OPEN")
 
 # @bot.message_handler(commands=['close'])
 def closechat(context: telegram.ext.CallbackContext):
@@ -28,27 +36,20 @@ def closechat(context: telegram.ext.CallbackContext):
     context.bot.send_message("-1001258717612", "Friday evening. Chat CLOSED")
 
 
-# @bot.message_handler(commands=['open'])
-# def openchat(message):
-#    bot.set_chat_permissions("-1001258717612", ChatPermissions(can_send_messages=True, can_send_media_messages=True,
-#                                                               can_send_polls=True, can_send_other_messages=True,
-#                                                               can_add_web_page_previews=True,
-#                                                               can_change_info=None, can_invite_users=True,
-#                                                               can_pin_messages=None))
-#    #    bot.reply_to(message, "Chat closed")
-#    bot.send_message("-1001258717612", "Monday morning. Chat OPEN")
 
 def check_date_and_time(context: telegram.ext.CallbackContext):
     thetime = datetime.datetime.now()
     print("Check called - hour now is: {} day num is: {}".format(thetime.hour, thetime.weekday()))
 
-    #    if (thetime.hour == 17)
+    if thetime.weekday() == 2 and thetime.hour == 17:
+        print("Found match: Friday 6pm, closing chat...")
+        context.bot.send_message(chat_id="-1001258717612", text='The time is NOW')
+    elif thetime.weekday() == 0 and thetime.hour == 8:
+        print("Found match: Monday 8am, opening chat...")
+        context.bot.send_message(chat_id="-1001258717612", text='The time is NOW')
 
-    context.bot.send_message(chat_id="-1001258717612", text='The time is now')
 
-
-j.run_repeating(check_date_and_time, 2)
-
+j.run_repeating(check_date_and_time, 5)
 
 u.start_polling()
 u.idle()
